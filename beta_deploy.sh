4c2b1a864372d3c6315cd9940e9be1dbf0e9eebd
Simple_xp <sunxinpeng1994@gmail.com>
2018-05-15 15:28:46 -0500

    Update beta_deploy.sh

--- beta_deploy.sh
+++ beta_deploy.sh
@@ -6,13 +6,19 @@
 # This will update the beta code at the rda subdomain
 #curl -s 'https://forge.laravel.com/servers/102862/sites/471050/deploy/http?token=MuYNzkjNrHfdZV3Cz3jXFRs6th0Fm3DFhxLxTAUc';
 #echo 'Deployment triggered!'
-cd /var/www/html/Metadata-Registry
-sudo git pull origin beta
 
-sudo composer install --no-interaction --prefer-dist --optimize-autoloader
-sudo php artisan migrate --force
-sudo php artisan view:clear
-sudo php artisan config:cache
-sudo yarn
-sudo npm run dev
+# cd /var/www/html/Metadata-Registry
+# sudo git pull origin beta
+
+# sudo composer install --no-interaction --prefer-dist --optimize-autoloader
+# sudo php artisan migrate --force
+# sudo php artisan view:clear
+# sudo php artisan config:cache
+# sudo yarn
+# sudo npm run dev
+
 #sudo php symfony cc
+
+
+cd /home/admin
+sudo git pull origin beta
